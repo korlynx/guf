3c194e3ccb89887919efbe233cb9ad5ccb031e5a
nonso <unaichi.chinonso@gmail.com>
2019-08-10 02:00:50 +0200

    new_version

--- group_backup.py
+++ group_backup.py
@@ -10,14 +10,34 @@ import numpy as np
 logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
                     filename="backup.log", level=logging.DEBUG)
 
-class getGroupMembersFiles(object):
+class GroupMembersFiles:
+
 
     def __init__(self):
         self.grp_names = input("enter group name/(s): ").split(" ")
         logging.info("input group name {}".format(str(self.grp_names)))
         
+        
+    def check_grp_names(self):
 
-    def get_and_check_grp_info(self):
+        for self.grp_name_ in self.grp_names:
+        
+            try:
+                self.group_info_ = grp.getgrnam(self.grp_name_)
+                print("{}".format(str(self.group_info_)))
+                logging.info("{}".format(str(self.group_info_)))
+
+            except KeyError:
+
+                print("input group name {} does not exist!".format(self.grp_name_))
+                logging.warning(
+                    "input group name {} does not exist!".format(self.grp_name_))
+                exit(1)
+                logging.info(
+                    "exit status 1 {} does not exist".format(self.grp_name_))
+
+
+    def get_unique_users(self):
         """"
         This function;
         - gets and prints out group infomations
@@ -25,28 +45,14 @@ class getGroupMembersFiles(object):
         """
 
         self.grp_mem_ = list()
-
         for grp_name in self.grp_names:
             self.grp_name = grp_name
-            try:
-                self.group_info_ = grp.getgrnam(self.grp_name)
-                print("{}".format(str(self.group_info_)))
-                logging.info("{}".format(str(self.group_info_)))
-
-            except KeyError:
-                print("input group name {} does not exist!".format(self.grp_name))
-                logging.warning(
-                    "input group name {} does not exist!".format(self.grp_name))
-                exit(1)
-                logging.info(
-                    "exit status 1 {} does not exist".format(self.grp_name))
-            self.grp_mem_.append(self.group_info_.gr_mem)
-
+            self.grp_mem_.append(grp.getgrnam(self.grp_name).gr_mem)
         
-        # gets unique list of group members to avoid duplicate backup
         self.grp_members = np.unique(
-            list(itertools.chain.from_iterable(self.grp_mem_)))
-        logging.info("generating unque users list from group name/(s)")
+            list(itertools.chain.from_iterable(self.grp_mem_)))  # gets unique list of group members to avoid duplicate backup
+        logging.info("generating unique users list from group name/(s)")
+        
         return self.grp_members
 
 
@@ -55,14 +61,14 @@ class getGroupMembersFiles(object):
         This function;
         - Gets backup confirmation from admin.
         """
-        self.get_ans = input(
+        self.get_con = input(
             "Do you want to continue to back up files for all group members? yes/no\n")
 
-        if self.get_ans == "yes":
+        if self.get_con == "yes":
             self.con_exit = 0
             logging.info("back up permission granted {}".format(str(self.con_exit)))
 
-        elif self.get_ans == "no":
+        elif self.get_con == "no":
             self.con_exit = 1
             logging.info("back up permission not granted {}".format(str(self.con_exit)))
             exit(1)
@@ -81,25 +87,17 @@ class getGroupMembersFiles(object):
         """
         self.os_dir = os_dir
         if not os.path.exists(self.os_dir):
-            print(self.os_dir, "does not exist.")
+            return 1
+        else:
+            return self.os_dir
 
-            self.get_con_ = input(
-                "Do you want this program to create {}? yes/no\n".format(self.os_dir))
 
-            if self.get_con_ == "yes":
-                print("creating new backup directory {}".format(str(self.os_dir)))
-                
-                self.new_dir = os.mkdir(self.os_dir)
-                logging.info("new directory {} created".format(str(self.new_dir)))
-                return self.new_dir
-            else:
-                
-                logging.warning(
-                    "exit status 1 {} does not exist".format(self.os_dir))
-                exit(1)
-        return self.os_dir
+    def creat_new_target_dir(self, os_dir):
 
-        
+        self.new_dir = os.mkdir(os_dir)
+        return self.new_dir
+
+
     def backup_group_user_files(self):
         """
         This function;
@@ -109,36 +107,50 @@ class getGroupMembersFiles(object):
         grp_names_ = self.grp_names
 
         # get group informations
-        group_info_ = self.get_and_check_grp_info()
+        group_info_ = self.check_grp_names()
+        grp_users = self.get_unique_users()
         logging.info(
             "list of users file to be archived: {}".format(str(group_info_)))
+        print(grp_users)
 
         # assert back up permission
         self.get_confirmation()
 
         # get back up directory as input from terminal
-        self.backup_target = input("enter target backup directory: ")
+        backup_target = input("enter target backup directory: ")
         logging.info("get target directory name from terminal")
+
         # check if directory exists, otherwise create target backup directory automatically
-        target_dir = self.check_backup_dir(self.backup_target)
+        target_dir = self.check_backup_dir(backup_target)
+
+        if target_dir == 1:
+            print(backup_target, "does not exist.")
+            self.get_con_ = input(
+                "Do you want this program to create {}? yes/no\n".format(backup_target))
+
+            if self.get_con_ == "yes":
+                print("creating new backup directory {}".format(backup_target))
+                target_dir = self.creat_new_target_dir(backup_target)
+            else:
+                exit(1)
 
         # copy members files to an archive directory
-        for self.user_name in group_info_:
-            self.res = subprocess.call(["cp", "-rf", "/home/"+self.user_name, target_dir])
+        for user_name in grp_users:
+            self.res = subprocess.call(["cp", "-rf", "/home/"+user_name, str(target_dir)])
 
             if self.res == 0:
                 logging.info(
-                    "back up {} files to an archive directory successful".format(str(self.user_name)))
+                    "back up {} files to an archive directory successful".format(user_name))
                 logging.info("exit status = {}".format(str(self.res)))
             else:
                 logging.info(
-                    "back up {} files to an archive directory failed".format(str(self.user_name)))
+                    "back up {} files to an archive directory failed".format(user_name))
                 logging.warning("exit status = {}".format(str(self.res)))
         print("back up completed succesfully")
 
 try:
 
-    group_names_input = getGroupMembersFiles()
+    group_names_input = GroupMembersFiles()
     group_names_input.backup_group_user_files()
 
 except KeyError:
