0f1470af2e7403f4e20e346ccc1d3f8ab3cc9b4c
nonso <unaichi.chinonso@gmail.com>
2019-08-14 18:29:21 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("READEME.md", "r") as fh:
+    long_description =fh.read()
+
+setuptools.setup(
+    name="gufs",
+    version="1.0.0",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    author_email="unaichi.chinonso@gmail.com",
+    long_description=long_description,
+    install_requires=['tqdm'],
+    classifiers=["Programming Language :: Python :: 3"],
+
+)
